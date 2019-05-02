2754ee1319f97c7bd22fdda159c3d48f33ae9bf2
ulqcifer7 <ciferulquiorra008@gmail.com>
2019-05-02 22:51:44 +0545

    recyclerview added

--- app/src/main/java/com/example/onlineshopping/Dashboard.java
+++ app/src/main/java/com/example/onlineshopping/Dashboard.java
@@ -3,15 +3,23 @@ package com.example.onlineshopping;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
 
     TextView displayUsername;
     Button btnLogout;
     String dUsername;
+    private RecyclerView recyclerView;
+    List<Items> itemsList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +31,13 @@ public class Dashboard extends AppCompatActivity {
         dUsername=getIntent().getStringExtra("USERNAME");
         displayUsername.setText(dUsername);
 
+        recyclerView=findViewById(R.id.recyclerView);
+
+        prepareItemsList();
+        recyclerView.setAdapter(new ItemsAdapter(itemsList,getApplicationContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,5 +46,10 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(logout);
             }
         });
+    }
+    public void prepareItemsList()
+    {
+        itemsList.add(new Items("shirt","Rs. 2000",R.drawable.kakashi));
+        itemsList.add(new Items("t-shirt","Rs. 1500",R.drawable.tg));
     }
 }
--- app/src/main/java/com/example/onlineshopping/ItemsAdapter.java
+++ app/src/main/java/com/example/onlineshopping/ItemsAdapter.java
@@ -4,6 +4,7 @@ import android.content.ClipData;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,8 +12,38 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class ItemsAdapter
+public class ItemsAdapter extends RecyclerView.Adapter <ItemsAdapter.ItemsViewHolder>
 {
+
+    List<Items> itemsList;
+    Context context;
+
+    public ItemsAdapter(List<Items> itemsList, Context context) {
+        this.itemsList = itemsList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view= LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_recyclerview,viewGroup,false);
+        return new ItemsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsViewHolder itemsViewHolder, int i) {
+
+        Items items=itemsList.get(i);
+        itemsViewHolder.iName.setText(items.getItemName());
+        itemsViewHolder.iPrice.setText(items.getItemPrice());
+        itemsViewHolder.iImage.setImageResource(items.getItemImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsList.size();
+    }
 
     public class ItemsViewHolder extends RecyclerView.ViewHolder
     {
